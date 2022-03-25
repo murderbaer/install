f00dc63fa7b4340bf5f2904b2dd07552894b8795
Arif Hasanic <arif_hasanic@outlook.com>
2022-03-25 15:19:25 +0100

    Update install_yay.sh

--- programms/pacman/install_yay.sh
+++ programms/pacman/install_yay.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-sudo pacman -S --noconfirm $(cat yaylist | cut -d' ' -f1)
-
-git clone https://github.com/Jguer/yay.git
-
-cd yay
-makepkg -si
-cd ..
-
+sudo yay -S --noconfirm $(cat yaylist | cut -d' ' -f1)
