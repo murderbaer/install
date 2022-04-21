be47cb70a5be97366695129d1ea1f84f3ed5436d
Arif Hasanic <arif.hasanic@locamo.de>
2022-04-21 16:06:47 +0200

    small config changes

--- programms/dwm/config.h
+++ programms/dwm/config.h
@@ -12,8 +12,8 @@ static const char dmenufont[]       = "Font Awesome 5 Free Solid:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray4[]       = "#444444";
+static const char col_cyan[]        = "#6792AC";
 
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
